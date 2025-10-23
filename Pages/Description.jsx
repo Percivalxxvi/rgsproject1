dc046fdaefc65cdcefcd651cff6736dc6af0550e
Percivalxxvi <ayodejioladeji12@gmail.com>
2025-10-23 15:15:58 +0100

    v

--- Pages/Description.jsx
+++ Pages/Description.jsx
@@ -9,26 +9,26 @@ const Description = () => {
     console.log(state)
   return (
     <div>
-        <div className='back'><Link to={"/"}> Back</Link></div>
-        <div className='desc'>
-            <div className='desc-img'>
-                <img src={state.strMealThumb} alt="" />
-            </div>
-            <div className='desc-txt'>
-                <h1>Name: {state.strMeal}</h1>
-                <h1>{state.strCategory}</h1>
-                <h1>{state.strArea}</h1>
-                <p>Instructions:{state.strInstructions}</p>    
-                <div className='desc-vid'>
-                    <h1>Watch Tutorial Here:</h1>
-                    <a href={state.strYoutube}>Youtube</a>  
-                </div>            
-                              
-            </div>
-            
+      <div className="back">
+        <Link to={"/"}> Back</Link>
+      </div>
+      <div className="desc">
+        <div className="desc-img">
+          <img src={state.strMealThumb} alt="" />
         </div>
+        <div className="desc-txt">
+          <h1 className="font-bold w-[95%]">Name: {state.strMeal}</h1>
+          <h1 className="w-[95%]">{state.strCategory}</h1>
+          <h1 className="w-[95%]">{state.strArea}</h1>
+          <p>Instructions:{state.strInstructions}</p>
+          <div className="desc-vid">
+            <h1>Watch Tutorial Here:</h1>
+            <a href={state.strYoutube}>Youtube</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Description
